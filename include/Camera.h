57968c30d6057207e2a602fe6defb14c22133ae7
Brycewan <bohuiwan@gmail.com>
2024-07-22 17:05:01 +0100

    complete Camera class

--- include/Camera.h
+++ include/Camera.h
@@ -1,5 +1,32 @@
 #pragma once
 
-class Camera {
+#include "Maths.h"
 
+class Camera {
+public:
+  Vec3 position;
+  Vec3 target;
+  Vec3 up;
+  float fov;
+  float aspect;
+  float zNear;
+  float zFar;
+
+  Camera(Vec3 pos = Vec3(0.0f, 0.0f, 3.0f),
+    Vec3 tar = Vec3(0.0f, 0.0f, 0.0f),
+    Vec3 upVec = Vec3(0.0f, 1.0f, 0.0f),
+    float fovAngle = 45.0f,
+    float aspectRatio = 1.0f / 1.0f,
+    float nearPlane = 0.1f,
+    float farPlane = 100.0f)
+    : position(pos), 
+      target(tar), 
+      up(upVec), 
+      fov(fovAngle), 
+      aspect(aspectRatio), 
+      zNear(nearPlane), 
+      zFar(farPlane) {}
+
+  Transform4f getViewMatrix() const;
+  Transform4f getProjectionMatrix() const;
 };
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,1 +1,9 @@
 #include "Camera.h"
+
+Transform4f Camera::getViewMatrix() const {
+  return Transform4f::lookat(position, target, up);
+}
+
+Transform4f Camera::getProjectionMatrix() const {
+  return Transform4f::perspective(fov, aspect, zNear, zFar);
+}
